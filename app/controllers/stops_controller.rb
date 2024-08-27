48ef6c2b1717256f9f909e84c7e1936e1c29aa3d
Lounis Boudjemia <lounisboudjemia@gmail.com>
2024-08-27 16:21:40 +0200

    updated stops controller

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -1,2 +1,51 @@
 class StopsController < ApplicationController
+  before_action :set_stop, only: [:show, :edit, :update, :destroy]
+  def index
+    @stops = Stop.all
+  end
+
+  def show
+  end
+
+  def new
+    @stop = Stop.new
+    @trip = Trip.find(params[:trip_id])
+  end
+
+  def create
+    @stop = Stop.new(stop_params)
+    @trip = Trip.find(params[:trip_id])
+    @stop.trip = @stop
+    if @stop.save
+      redirect_to trip_path(@trip), status: :see_other
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @stop.update(stop_params)
+      redirect_to @stop, notice: 'You updated this stop successfully.'
+    else
+      render :show
+    end
+  end
+
+  def destroy
+    @stop.destroy
+    redirect_to stops_path(@stop)
+  end
+
+  private
+
+  def stop_params
+    params.require(:stop).permit(:name, :start_date, :end_date,:address, :trip_id)
+  end
+
+  def set_stop
+    @stop = Stop.find(params[:id])
+  end
 end
