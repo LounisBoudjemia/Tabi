820b466a1bb08df075374b6f3ef7b3eacf04cc0a
Lounis Boudjemia <lounisboudjemia@gmail.com>
2024-08-27 14:16:47 +0200

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,26 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :trips do
+    resources :diary_entries, shallow: true
+    resources :checklists, shallow: true
+    resources :stops, shallow: true do
+      resources :activities, only: [:new, :create, :edit, :update, :destroy], shallow: true
+    end
+  end
+
+  resources :checklist_templates do
+    resources :checklist_items,  only: [:new, :create, :edit, :update, :destroy], shallow: true do
+      resources :items, only: [:new, :create, :edit, :update, :destroy], shallow: true
+    end
+  end
+
+  # resources :stops, only: [] do
+  #   resources :activities, only: [:new, :create]
+  # end
+  # resources :activities, only: [:edit, :update, :destroy]
+
+  # resources :activities
   # Defines the root path route ("/")
   # root "posts#index"
 end
