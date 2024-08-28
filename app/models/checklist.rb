de68991cdeb1a360232758ef8c7ad18d1031e85b
Lounis Boudjemia <lounisboudjemia@gmail.com>
2024-08-28 11:52:56 +0200

    validation updates

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -1,6 +1,6 @@
 class Checklist < ApplicationRecord
   belongs_to :trip
-  has many :checklist_items, as: :checklistable
+  has_many :checklist_items, as: :checklistable
 
   validates :name, presence: true
 end
