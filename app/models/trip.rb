d3b9d3e0885c6a2c3db39c4cb213c9d58aebae39
Lounis Boudjemia <lounisboudjemia@gmail.com>
2024-08-28 11:29:26 +0200

    updated show functionnalities

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,7 @@
 class Trip < ApplicationRecord
+  has_one_attached :photo
   has_many :stops
   has_many :checklists
-  has_one_attached :photo
 
   validates :name, presence: true
   validates :start_date, presence: true
