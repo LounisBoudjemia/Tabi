d707814d4085cb1401e8628b9060ffac60ae23b7
Lounis Boudjemia <lounisboudjemia@gmail.com>
2024-08-29 17:56:27 +0200

    wip, set up cards but need to adjust grids

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -46,7 +46,7 @@ class TripsController < ApplicationController
     @trip.destroy
     redirect_to trips_path(@trip)
   end
-
+  
   private
 
   def trip_params
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,10 +18,10 @@ User.create!(email: "florian@florian.com", password: "password")
 User.create!(email: "frida@frida.com", password: "password")
 User.create!(email: "navid@navid.com", password: "password")
 
-japan = Trip.create!(name: "Japan", start_date: "2023-09-05", end_date: "2023-09-30")
-france = Trip.create!(name: "France", start_date: "2024-09-14", end_date: "2024-10-09")
+# japan = Trip.create!(name: "Japan", start_date: "2023-09-05", end_date: "2023-09-30")
+# france = Trip.create!(name: "France", start_date: "2024-09-14", end_date: "2024-10-09")
 
-hiro = Stop.create!(trip: japan, name: "Hiroshima", start_date: "2023-09-06", end_date: "2023-09-09", address: "3 Chome-1-1 Otemachi, Naka Ward, Hiroshima, 730-0051, Japan")
+# hiro = Stop.create!(trip: japan, name: "Hiroshima", start_date: "2023-09-06", end_date: "2023-09-09", address: "3 Chome-1-1 Otemachi, Naka Ward, Hiroshima, 730-0051, Japan")
 
 # paris = Stop.create!(trip: france, name: "Paris", start_date: "2024-09-14", end_date: "2024-09-20", address: "55, avenue des champs élysées Paris")
 
@@ -35,21 +35,21 @@ hiro = Stop.create!(trip: japan, name: "Hiroshima", start_date: "2023-09-06", en
 #                 start_date:"2024-09-16", favorite: true, category: "amusement park",
 #                 description: "Day trip at Disneyland Paris. Opens at 9.")
 
-Stop.create!(trip: japan, name: "Aso", start_date: "2023-09-09", end_date: "2023-09-12",
-             address: "1506 Otohime, Aso, Kumamoto 869-2226, Japon")
+# Stop.create!(trip: japan, name: "Aso", start_date: "2023-09-09", end_date: "2023-09-12",
+#              address: "1506 Otohime, Aso, Kumamoto 869-2226, Japon")
 
 
-Stop.create!(trip: japan, name: "Fukuoka", start_date: "2023-09-12", end_date: "2023-09-15",
-             address: "Fukuoka")
+# Stop.create!(trip: japan, name: "Fukuoka", start_date: "2023-09-12", end_date: "2023-09-15",
+#              address: "Fukuoka")
 
-Stop.create!(trip: japan, name: "Okinawa", start_date: "2023-09-15", end_date: "2023-09-21",
-             address: "938 Ishikawa, 本部町 Motobu, Kunigami District, Okinawa 905-0206, Japon")
+# Stop.create!(trip: japan, name: "Okinawa", start_date: "2023-09-15", end_date: "2023-09-21",
+#              address: "938 Ishikawa, 本部町 Motobu, Kunigami District, Okinawa 905-0206, Japon")
 
-Stop.create!(trip: japan, name: "Nagoya", start_date: "2023-09-21", end_date: "2023-09-24",
-             address: "3 Chome-1-22 Nishiki, Naka Ward, Nagoya, Aichi 460-0003, Japon")
+# Stop.create!(trip: japan, name: "Nagoya", start_date: "2023-09-21", end_date: "2023-09-24",
+#              address: "3 Chome-1-22 Nishiki, Naka Ward, Nagoya, Aichi 460-0003, Japon")
 
-Stop.create!(trip: japan, name: "Tokyo", start_date: "2023-09-24", end_date: "2023-09-30",
-             address: "Japon, 〒154-0001 Tokyo, Setagaya City, Ikejiri, 2 Chome−26−5 池尻網野レジデンス")
+# Stop.create!(trip: japan, name: "Tokyo", start_date: "2023-09-24", end_date: "2023-09-30",
+#              address: "Japon, 〒154-0001 Tokyo, Setagaya City, Ikejiri, 2 Chome−26−5 池尻網野レジデンス")
 
 # Activity.create!(stop: hiro, name: "peace memorial museum",
 #                  start_date:"2023-09-07", favorite: true, category:"museum",
