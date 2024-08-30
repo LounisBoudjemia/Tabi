bbe842a4cfc4d909dd13773be6631379f9609632
Florian Sitte <114937633+StarTyper@users.noreply.github.com>
2024-08-30 17:37:36 +0200

    work from friday - not done yet

--- app/controllers/checklists_controller.rb
+++ app/controllers/checklists_controller.rb
@@ -1,2 +1,7 @@
 class ChecklistsController < ApplicationController
+  def index
+    @trip = Trip.find(params[:trip_id])
+    @checklists = Checklist.all
+    @checklist_teamplates = ChecklistTemplate.all
+  end
 end
