7834a0efcd97eca07ae70616acefc3dcdb0b5a75
Lounis Boudjemia <lounisboudjemia@gmail.com>
2024-09-03 16:08:51 +0200

    styling underway but almost done

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,7 +1,9 @@
 class TripsController < ApplicationController
   before_action :set_trip, only: [:show, :edit, :update, :destroy]
   def index
-    @trips = Trip.all
+    @trips = Trip.all.order(start_date: :asc)
+    @upcoming_trips = @trips.partition { |trip| trip.start_date > Date.today }
+    @old_trips = @trips.partition { |trip| trip.end_date < Date.today }
   end
 
   def show
