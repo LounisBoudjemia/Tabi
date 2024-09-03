5cbbad2f285cedde65d77d466ae1ad4456928786
Florian Sitte <114937633+StarTyper@users.noreply.github.com>
2024-09-03 15:46:23 +0200

    create and delete checklists and templates

--- app/controllers/checklist_templates_controller.rb
+++ app/controllers/checklist_templates_controller.rb
@@ -1,5 +1,48 @@
 class ChecklistTemplatesController < ApplicationController
+  def new
+    @trip = Trip.find(params[:trip_id])
+    # @user = current_user
+    @checklist_template = ChecklistTemplate.new
+  end
+
+  def create
+    @trip = Trip.find(params["trip_id"])
+    @checklist_template = ChecklistTemplate.new(name: params[:checklist_template][:name], user_id: current_user.id)
+
+    if @checklist_template.save
+      redirect_to trip_checklists_path(trip_id: @trip.id)
+    else
+      render :new
+    end
+  end
+
   def show
+    @trip = Trip.find(params[:trip_id])
     @checklist_template = ChecklistTemplate.find(params[:id])
+  end
+
+  def destroy
+    @trip = Trip.find(params[:trip])
+    @checklist_template = ChecklistTemplate.find_by(id: params[:id])
+    @checklist_items = ChecklistItem.where(checklistable_type: "ChecklistTemplate", checklistable_id: @checklist_template.id)
+
+    @checklist_items.each do |checklist_item|
+      item = Item.find(checklist_item.item_id)
+      checklist_item.destroy
+      item.destroy if ChecklistItem.where(item_id: item.id).empty?
+    end
+
+    respond_to do |format|
+      if @checklist_template.destroy
+        format.html { redirect_to trip_checklists_path(trip_id: @trip.id) }
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(:template_container, partial: 'checklist_templates/template_container',
+                                                    locals: { checklist_templates: ChecklistTemplate.all, trip: @trip })
+        end
+      else
+        format.html { render :new }
+        format.turbo_stream { render :new }
+      end
+    end
   end
 end
--- app/controllers/checklists_controller.rb
+++ app/controllers/checklists_controller.rb
@@ -6,12 +6,53 @@ class ChecklistsController < ApplicationController
     @checklist_templates = ChecklistTemplate.where(user: current_user)
   end
 
+  def new
+    @trip = Trip.find(params[:trip_id])
+    @checklist = Checklist.new
+  end
+
+  def create
+    @trip = Trip.find(params["trip_id"])
+    @checklist = Checklist.new(name: params[:checklist][:name], trip_id: @trip.id)
+
+    if @checklist.save
+      redirect_to trip_checklists_path(trip_id: @trip.id)
+    else
+      render :new
+    end
+  end
+
   def show
     @trip = Trip.find(params[:trip_id])
     @checklist = Checklist.find(params[:id])
     respond_to do |format|
       format.turbo_stream
       format.html # fallback if needed
+    end
+  end
+
+  def destroy
+    @trip = Trip.find(params[:trip])
+    @checklist = Checklist.find_by(id: params[:id])
+    @checklist_items = ChecklistItem.where(checklistable_type: "Checklist", checklistable_id: @checklist.id)
+
+    @checklist_items.each do |checklist_item|
+      item = Item.find(checklist_item.item_id)
+      checklist_item.destroy
+      item.destroy if ChecklistItem.where(item_id: item.id).empty?
+    end
+
+    respond_to do |format|
+      if @checklist.destroy
+        format.html { redirect_to trip_checklists_path(trip_id: @trip.id) }
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(:checklist_container, partial: 'checklists/checklist_container',
+                                                    locals: { checklists: Checklist.all, trip: @trip })
+        end
+      else
+        format.html { render :new }
+        format.turbo_stream { render :new }
+      end
     end
   end
 end
