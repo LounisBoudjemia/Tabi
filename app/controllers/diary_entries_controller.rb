576a24993782dea8e396365ead3a716ac6ccd77b
Florian Sitte <114937633+StarTyper@users.noreply.github.com>
2024-09-04 16:16:55 +0200

    done

--- app/controllers/diary_entries_controller.rb
+++ app/controllers/diary_entries_controller.rb
@@ -2,15 +2,11 @@ class DiaryEntriesController < ApplicationController
   def index
     @trip = Trip.find(params[:trip_id])
     @stops = @trip.stops
+    @current_stops = @stops.select { |stop| stop.includes_date?(Date.today) }
     @diary_entries = @trip.diary_entries
-    @diary_entry = @diary_entries.first
+    # @diary_entry = @diary_entries.first
     @events = @stops + @diary_entries
     @show_diary_entry = DiaryEntry.find_by(date: Date.today)
-    # if DiaryEntry.find_by(date: Date.today).present?
-    #   @show_diary_entry = DiaryEntry.find_by(date: Date.today)
-    # else
-    #   @show_diary_entry = DiaryEntry.create(date: Date.today, headline: "Entry from #{Date.today}")
-    # end
     @diary_entry = DiaryEntry.new
     @date = params[:date] || Date.today
   end
@@ -24,17 +20,9 @@ class DiaryEntriesController < ApplicationController
     @show_diary_entry = DiaryEntry.find_by(date: params["date"], trip_id: @trip.id)
     @diary_entry = DiaryEntry.new
     @date = params[:date] || Date.today
-    # trip_start = @trip.start_date
-    # start_date = Date.parse(params[:date])
-    #   loop do
-    #     @stop = Stop.find_by(trip_id: @trip, start_date: start_date)
-
-    #     break unless @stop.nil?
-
-    #     start_date -= 1.day
-
-    #     break if  trip_start = start_date - 1.day
-    #   end
+    @calendar_date = calendar_date = Date.parse(params[:date])
+    @stops = @trip.stops
+    @current_stops = @stops.select { |stop| stop.includes_date?(@calendar_date) }
   end
 
   def create
