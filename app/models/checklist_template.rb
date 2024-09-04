1c1a0f9f5457b34fa7773840c457688dc7c16424
Florian Sitte <114937633+StarTyper@users.noreply.github.com>
2024-09-04 23:03:11 +0200

    work in progress

--- app/models/checklist_template.rb
+++ app/models/checklist_template.rb
@@ -4,4 +4,23 @@ class ChecklistTemplate < ApplicationRecord
   has_many :items, through: :checklist_items
 
   validates :name, presence: true
+
+  def separate_checked_and_unchecked_items
+    # Eager load the associated checklist items and items to reduce database queries.
+    checklist_items_with_items = checklist_items.includes(:item)
+
+    unchecked_items = []
+    checked_items = []
+
+    # Separate items based on the checked attribute
+    checklist_items_with_items.each do |checklist_item|
+      if checklist_item.checked
+        checked_items << checklist_item.item
+      else
+        unchecked_items << checklist_item.item
+      end
+    end
+
+    [unchecked_items, checked_items]
+  end
 end
