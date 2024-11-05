eb87d01e43889e600ba1b1a1e76fe0c345fbf0cc
Lounis Boudjemia <lounisboudjemia@gmail.com>
2024-11-05 06:53:52 +0100

    share popup updated

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,7 +3,7 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
-const viewBtn = document.querySelector("#share"),
+const viewBtn = document.querySelector("#trip-icon-share"),
     popup = document.querySelector(".popup"),
     close = popup.querySelector(".close"),
     field = popup.querySelector(".field"),
@@ -16,9 +16,9 @@ const viewBtn = document.querySelector("#share"),
     });
 
 
-    copy.onclick = ()=>{
+    copy.addEventListener("click", (event) => {
       input.select(); //select input value
-      if(document.execCommand("copy")){ //if the selected text is copied
+      if(document.querySelector("copy")){ //if the selected text is copied
         field.classList.add("active");
         copy.innerText = "Copied";
         setTimeout(()=>{
@@ -26,14 +26,5 @@ const viewBtn = document.querySelector("#share"),
           field.classList.remove("active");
           copy.innerText = "Copy";
         }, 3000);
-      }
-    }
-
-    btn.addEventListener("click", async () => {
-      try {
-        await navigator.share(shareData);
-        resultPara.textContent = "Trip shared successfully";
-      } catch (err) {
-        resultPara.textContent = `Error: ${err}`;
       }
     });
