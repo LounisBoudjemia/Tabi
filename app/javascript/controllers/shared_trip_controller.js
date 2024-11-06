098e6dcc83ea9e69a3f529c7b0e7cb174d37ecf5
Lounis Boudjemia <lounisboudjemia@gmail.com>
2024-11-06 15:01:09 +0100

    fixed pop up

--- app/javascript/controllers/shared_trip_controller.js
+++ app/javascript/controllers/shared_trip_controller.js
@@ -1,32 +1,35 @@
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import { Controller } from "@hotwired/stimulus"
 
-// const viewBtn = document.querySelector("#trip-icon-share"),
-//     popup = document.querySelector(".popup"),
-//     close = popup.querySelector(".close"),
-//     field = popup.querySelector(".field"),
-//     input = field.querySelector("input"),
-//     copy = field.querySelector("button");
+export default class extends Controller {
 
-
-//     viewBtn.addEventListener("click", (event) => {
-//       popup.classList.toggle("show");
-//     });
-
-//     // close.addEventListener("click", (event) => {
-//     //   close.close();
-//     // });
+  connect() {
+    const viewBtn = document.querySelector("#trip-icon-share"),
+    popup = document.querySelector(".popup"),
+    close = popup.querySelector(".close"),
+    field = popup.querySelector(".field"),
+    input = field.querySelector("input"),
+    copy = field.querySelector("button");
 
 
-//     copy.addEventListener("click", (event) => {
-//      if (input.select()); //select input value
-//       navigator.clipboard.writeText(input.value);
-//       field.classList.add("active");
-//       copy.innerText = "Copied";
-//       setTimeout(()=>{
-//         window.getSelection().removeAllRanges(); //remove selection from page
-//         field.classList.remove("active");
-//         copy.innerText = "Copy";
-//       }, 3000);
-//     });
+    viewBtn.addEventListener("click", (event) => {
+      popup.classList.toggle("show");
+    });
+
+    close.addEventListener("click", (event) => {
+      viewBtn.click();
+    });
+
+
+    copy.addEventListener("click", (event) => {
+     if (input.select()); //select input value
+      navigator.clipboard.writeText(input.value);
+      field.classList.add("active");
+      copy.innerText = "Copied";
+      setTimeout(()=>{
+        window.getSelection().removeAllRanges(); //remove selection from page
+        field.classList.remove("active");
+        copy.innerText = "Copy";
+      }, 3000);
+    });
+  }
+}
